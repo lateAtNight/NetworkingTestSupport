26b34bd71c81f6f8ec101de3f1a43b421acf0aba
sam woolf <sam.woolf@karmarama.com>
2021-01-15 11:40:25 +0000

    make regex match entire range else fail

--- Sources/NetworkingTestSupport/TestURLSession.swift
+++ Sources/NetworkingTestSupport/TestURLSession.swift
@@ -354,6 +354,7 @@ private extension String {
     static func ~= (lhs: String, rhs: String) -> Bool {
         guard let regex = try? NSRegularExpression(pattern: rhs) else { return false }
         let range = NSRange(location: 0, length: lhs.utf16.count)
-        return regex.firstMatch(in: lhs, options: [], range: range) != nil
+		guard let match = regex.firstMatch(in: lhs, options: [], range: range) else { return false }
+		return range == match.range
     }
 }
